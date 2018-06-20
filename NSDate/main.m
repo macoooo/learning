1b090dcb9ceacd776a99054ba4147a297fcb6380
强淑婷 <qiangshuting@qiangshutingdemacbook-air.local>
2018-06-20 21:17:49 +0800

    Initial Commit

---
+++ NSDate/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSDate
+//
+//  Created by 强淑婷 on 2018/6/20.
+//  Copyright © 2018年 强淑婷. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
